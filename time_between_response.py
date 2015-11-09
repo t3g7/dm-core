1efcbf5500215bb710212b630c844ec8bbcd17a8
mgasque <maxime.gasque@gmail.com>
2015-11-09 15:08:33 +0100

    temps réponse sav sosh fonctionnel

--- time_between_response.py
+++ time_between_response.py
@@ -5,34 +5,32 @@ from datetime import datetime
 from datetime import timedelta
 
 # Recup des tweets en json
-tab = []
 dates = []
 tweet_id = []
-with open('datasets/tweets_dataset_20151027.txt',"r") as data_file:   
+with open('tweets_dataset/tweets.txt',"r") as data_file:   
 	for line in data_file: 
 		data = json.loads(line)
-		data2 = data['entities']['user_mentions']
-		for line in data2:
-			if data['in_reply_to_status_id'] != None and line['screen_name'] == "Orange_France":
-				tweet_id = tweet_id + [data['in_reply_to_status_id']]
-				time = strptime(data['created_at'],"%a %b %d %H:%M:%S +0000 %Y")
-				dt = datetime.fromtimestamp(mktime(time))
-				dates = dates + [dt]
-				# tab = tab + [data['created_at']]
-# print tweet_id
+		if 'text' in data:
+			data2 = data.get('user').get('screen_name')
+			if data2 is not None and len(data2) is not 0:
+			 	if data.get('in_reply_to_status_id') and data2 == u'Sosh_fr':
+			 		tweet_id = tweet_id + [data.get('in_reply_to_status_id')]
+			 		time = strptime(data['created_at'],"%a %b %d %H:%M:%S +0000 %Y")
+			 		dt = datetime.fromtimestamp(mktime(time))
+			 		dates = dates + [dt]
 
 heure_tweet3 = []
 t = []
-with open('datasets/tweets_dataset_20151027.txt',"r") as data_file:   
+with open('tweets_dataset/tweets.txt',"r") as data_file:   
 	for line in data_file: 
 		data = json.loads(line)
 		for i in range(len(dates)):
-			if(tweet_id[i] == data['id']):
+			if(tweet_id[i] == data.get('id')):
 				heure_tweet = strptime(data['created_at'],"%a %b %d %H:%M:%S +0000 %Y")
 				heure_tweet2 = datetime.fromtimestamp(mktime(heure_tweet))
 				delt = dates[i] - heure_tweet2
 				heure_tweet3 = [delt] + heure_tweet3
 
-
-delta = sum(heure_tweet3, timedelta()) / len(heure_tweet3)
-print str(delta)
+if len(heure_tweet3) is not 0 :
+	delta = sum(heure_tweet3, timedelta()) / len(heure_tweet3)
+	print str(delta)
